65bf80e9c18bf3f0baa1c215fb762f70bedaac19
usuario <usuario@ubuntu-22.04>
2023-10-19 15:42:26 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println(System.getenv("PATH"));
+    System.out.println(System.getenv("HOME"));
+
+    System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
+    FirefoxOptions options = new FirefoxOptions();
+  
+    WebDriver driver = new FirefoxDriver(options);
+    driver.get("https://www.google.com");
+  }
+}
