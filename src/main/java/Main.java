0a450a607596300ef586de0ab7d02fb8d53f98f9
hammad20039 <muhammad@elpuig.xeill.net>
2023-10-25 17:56:12 +0200

    CurseForge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
@@ -10,9 +16,19 @@ public class Main {
 
     System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
     FirefoxOptions options = new FirefoxOptions();
+    options.setBinary("/usr/bin/firefox");
   
     WebDriver driver = new FirefoxDriver(options);
     driver.get("https://legacy.curseforge.com/");
+
+    WebDriverWait wait = new WebDriverWait(driver, 10);
+
+    WebElement tarjeta = driver.findElement(By.className("flex-steady"));
+   // wait.until(ExpectedConditions.elementToBeClickable(tarjeta));
+    tarjeta.click();
+
+//    List<WebElement> elements = driver.findElements(By.className("flex-steady"));
+
 
   }
 }
